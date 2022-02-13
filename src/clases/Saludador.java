479cf6fdd9ba8de47c2284af96bdd1a2e1e2a4a8
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-02-13 21:09:59 +0100

    Ej1

--- src/clases/Saludador.java
+++ src/clases/Saludador.java
@@ -1,5 +1,14 @@
 package clases;
 import javax.swing.*;
-public class Saludador {
-
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+public class Saludador extends JFrame{
+	private JPanel panel;
+	
+	public Saludador() {
+		setTitle("Saludador");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+        panel.setSize(300, 300); 
+        setVisible(true);
+	}
 }
