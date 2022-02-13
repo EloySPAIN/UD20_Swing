38eaf980b33642810922e5ec936ab6d76b1ca33f
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-02-13 21:58:44 +0100

    Ej1

--- src/clases/Saludador.java
+++ src/clases/Saludador.java
@@ -2,9 +2,19 @@ package clases;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+/**
+ * 
+ * @author Eloy Altozano
+ *
+ */
 public class Saludador extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	//Creamos las variables
 	private JPanel panel;
-	
+	JTextField texto = new JTextField(10);
+	JLabel lb = new JLabel("Escribe un nombre para saludar");
+	JButton boton = new JButton("Saludar!");
 	public Saludador() {
 		//Creamos el panel
 		setTitle("Saludador");
@@ -17,18 +27,23 @@ public class Saludador extends JFrame{
 		//Indicamos el diseño del panel
 		panel.setLayout(null);
 		setContentPane(panel);
-		JTextField texto = new JTextField(10);
-		JLabel lb = new JLabel("Escribe un nombre para saludar");
-		JButton boton = new JButton("Saludar!");
 		
 		//Asignamos las cardinalidades de los componentes
 		lb.setBounds(200,70,500,20);
-		texto.setHorizontalAlignment(texto.CENTER);//Ponemos el texto en el centro
+		texto.setHorizontalAlignment(SwingConstants.CENTER);//Ponemos el texto en el centro
 		texto.setBounds(200,100,190,20);
 		boton.setBounds(200,160,190,20);
 		//Lo añadimos al panel
 		panel.add(lb);
 		panel.add(texto);
 		panel.add(boton);
+		boton.addActionListener(new Resultado());
+	}
+	//Creamos el evento del boton
+	public class Resultado implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(null, "Hola " + texto.getText());
+		}
+		
 	}
 }
