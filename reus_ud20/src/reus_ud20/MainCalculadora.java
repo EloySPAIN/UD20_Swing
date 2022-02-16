15295f6d7ff5fdce324d7de3440ed4209bda76ba
Samuel Zecirovic <55589454+SamuelZecirovic@users.noreply.github.com>
2022-02-16 18:31:09 +0100

    Add files via upload

---
+++ reus_ud20/src/reus_ud20/MainCalculadora.java
@@ -0,0 +1,11 @@
+package reus_ud20;
+
+
+public class MainCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		calculadora.setVisible(true);
+	}
+	
+}
